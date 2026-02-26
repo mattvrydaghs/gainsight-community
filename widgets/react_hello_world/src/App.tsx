35c201377a5bedc673e45f1f4eb14587dfc94764
Matt Vrydaghs <matt.vrydaghs@gainsight.com>
2026-02-26 10:37:10 -0500

    test

--- widgets/react_hello_world/src/App.tsx
+++ widgets/react_hello_world/src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
     <div className="hello-world-container">
       <div className="hello-world-card">
         <div className="hello-world-glow" />
-        <h1 className="hello-world-title">Hello World</h1>
+        <h1 className="hello-world-title">Hello World!!!</h1>
         <div className="hello-world-divider" />
         <p className="hello-world-subtitle">Welcome to your first widget</p>
       </div>
